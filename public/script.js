5b2f2c4289196ece70bdc970a467f03cc22e1677
jorgelinasigal <116228030+jorgelinasigal@users.noreply.github.com>
2024-08-06 23:00:59 -0300

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var images = document.querySelectorAll(".image-container img");
+  var index = 0; // Asegúrate de declarar e inicializar la variable index
+
+  // Mostrar la primera imagen inicialmente
+  images.forEach(function (image) {
+    image.style.display = "none"; // Oculta todas las imágenes
+  });
+  images[index].style.display = "block"; // Muestra la primera imagen
+
+  setInterval(function () {
+    images.forEach(function (image) {
+      image.style.display = "none"; // Oculta todas las imágenes
+    });
+
+    images[index].style.display = "block"; // Muestra la siguiente imagen
+
+    index = (index + 1) % images.length; // Incrementa el índice, volviendo al inicio si es necesario
+  }, 2000); // Cambia la imagen cada 2 segundos
+});
